0f10ee7dcc55793d7a1174b4362a5474bb5dfcde
Gong-hub <1426911884@qq.com>
2021-04-03 20:21:30 +0800

    垃圾网

--- test.py
+++ test.py
@@ -47,7 +47,7 @@ def sendTg(tgBot, content):
         # chat_id = os.environ.get('TG_USERID')
         url = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}'
         session = requests.Session()
-        resp = session.post(url,data={"document":content})
+        resp = session.post(url,data={"text":content})
         print(resp)
     except Exception as e:
         print('Tg通知推送异常，原因为: ' + str(e))
