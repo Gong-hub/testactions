fa083d0584d92d4eeae4a9ae02185c982cee6194
Gong-hub <63841510+Gong-hub@users.noreply.github.com>
2021-04-03 19:01:05 +0800

    Update test.py
    a

--- test.py
+++ test.py
@@ -36,7 +36,7 @@ def sendTg(tgBot, content):
         #token = os.environ.get('TG_TOKEN')
         #用户的ID
         #chat_id = os.environ.get('TG_USERID')
-        url = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={content}'
+        url = f'https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&document={content}'
         session = requests.Session()
         resp = session.post(url)
         print(resp)
