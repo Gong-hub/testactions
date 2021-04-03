83ee6089e09971bdad2f7465ba526223926a3cb9
Gong-hub <1426911884@qq.com>
2021-04-03 08:06:50 +0800

    增加了test.py

---
+++ test.py
@@ -0,0 +1,9 @@
+# _*_ coding: utf-8 _*_
+# @Project_Name : testactions
+# @Time         : 2021/4/3 8:06
+# @Author       : Gong-hub
+# @Email        : 2821813806@qq.com
+# @File         : test.py
+# @Software     : PyCharm
+
+print("你好")
